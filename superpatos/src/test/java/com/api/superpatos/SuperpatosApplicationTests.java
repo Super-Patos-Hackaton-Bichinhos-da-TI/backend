6b1c26974afb40ace1b4abc8732d276359141113
Gabriel <gabrielsrmj@alu.ufc.br>
2024-06-14 10:04:10 -0300

    init java project

---
+++ superpatos/src/test/java/com/api/superpatos/SuperpatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.superpatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperpatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
